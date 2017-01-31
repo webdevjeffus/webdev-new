102f41cb1a1c32c09be9daa5426d252b79843a3f
Jeff George <webdevjeffus@gmail.com>
2017-01-30 21:54:45 -0500

    Implement working version of Tweet button on quote-monster.html

--- work/js/quote-monster.js
+++ work/js/quote-monster.js
@@ -25,8 +25,8 @@ $("#tweetBtn").on("click", function( event ) {
 
   $("#tweetBtn").html("<span class='fa fa-exclamation-triangle' aria-hidden='true'></span> Not yet!");
 
-  var quote = $('#quote'),
-      author = $('#author');
+  var quote = $("#quote"),
+      author = $("#author");
 
   if( author.text() === "QuoteMonster" ) {
     quote.text("You need to click \u2018Get a Quote\u2019 before you can \u2018Tweet it!\u2019");
@@ -39,43 +39,38 @@ $("#tweetBtn").on("click", function( event ) {
   }
 
   else {
-    function prepText(text) {
+    function queryText(text) {
       text = text.split("");
-      var punctuations = ".,!? s";
+      // var punctuations = ".,!? s";
 
       for( var i = 0; i < text.length; i++) {
-        if ( (text[i] === "'" && i === 0) ||
-             (text[i] === "'" && text[i-1]) === " " ||
-             (text[i] === '"' && i === 0) ||
-             (text[i] === '"' && text[i-1]) === " " ) {
-          text[i] = "X";
-        }
-        if ( (text[i] === "'" && i === text.length-1) ||
-             (text[i] === "'" && !(punctuations.includes(text[i+1])))  ||
-             (text[i] === '"' && i === text.length-1) ||
-             (text[i] === '"' && !(punctuations.includes(text[i+1]))) ) {
-          text[i] = "\u2018";
-        }
-        if ( text[i] === ";" && text[i+1] === " ") {
-          text[i] = "\u2014";
-          text[i+1] = "";
-        }
-        if ( text[i] === "+" ) { text[i] = "%2B"; }
-        if ( text[i] === " " ) { text[i] = "%20"; }
+      //   if ( (text[i] === "'" && i === 0) ||
+      //        (text[i] === "'" && text[i-1]) === " " ||
+      //        (text[i] === '"' && i === 0) ||
+      //        (text[i] === '"' && text[i-1]) === " " ) {
+      //     text[i] = "%E2%80%99";
+      //   }
+      //   else if ( (text[i] === "'" && i === text.length-1) ||
+      //        (text[i] === "'" && !(punctuations.includes(text[i+1])))  ||
+      //        (text[i] === '"' && i === text.length-1) ||
+      //        (text[i] === '"' && !(punctuations.includes(text[i+1]))) ) {
+      //     text[i] = "\u2018";
+      //   }
+        if ( text[i] === ";" ) { text[i] = "%3B"; }
+        else if ( text[i] === "+" ) { text[i] = "%2B"; }
+        else if ( text[i] === " " ) { text[i] = "%20"; }
       }
       return text.join("");
     }
 
-    var preppedQuote = prepText( $('#quote').text() ),
-        preppedAuthor = prepText( $('#author').text() );
+    var queryQuote = queryText( $("#quote").text() ),
+        queryAuthor = queryText( $("#author").text() );
 
-
-    var tweetHref = "https://twitter.com/intent/tweet?text=" + preppedAuthor + ":%20" + preppedQuote;
+    var tweetHref = "https://twitter.com/intent/tweet?text=" + queryQuote + "%E2%80%94" + queryAuthor;
 
     console.log( tweetHref );
 
     window.open( tweetHref );
-
   }
 
 });
