ee3aa56226b126261ea7110fe8b4af315c64b21a
Jeff George <webdevjeffus@gmail.com>
2017-01-30 22:23:51 -0500

    Implement prepText() in quote-monster.js to smartify quotes

--- work/js/quote-monster.js
+++ work/js/quote-monster.js
@@ -1,6 +1,29 @@
 $("#quoteBtn").on("click", function(event) {
   event.preventDefault();
 
+  function prepText(text) {
+      text = text.split("");
+      var punctuations = ".,!? s";
+
+      for( var i = 0; i < text.length; i++) {
+        // Open quotes - convert all to left single quote
+        if ( (text[i] === "'" && i === 0) ||            // Single quote, 1st char
+             (text[i] === "'" && text[i-1]) === " " ||  // Single quote preceded by space
+             (text[i] === '"' && i === 0) ||            // Double quote, 1st char
+             (text[i] === '"' && text[i-1]) === " " ||  // Double quote preceded by space
+             (text[i] === "\u201C") ) {                 // Left double quote
+          text[i] = "\u2018";                           // Convert to left single quote
+        }
+        // Close quotes and apostrophes - convert all to right single quote
+        if ( (text[i] === "'" ) ||                      // Single quote
+             (text[i] === '"' ) ||                      // Double quote
+             (text[i] === "\u201D") ) {                 // Right double quote
+          text[i] = "\u2019";                           // Convert to right single quote
+        }
+      }
+      return text.join("");
+    }
+
   $("#sorryBox").addClass("hidden");
   $("#quoteBox").removeClass("hidden");
   $("#quote-btn").html("Get another quote");
@@ -11,8 +34,8 @@ $("#quoteBtn").on("click", function(event) {
     url: "http://quotes.stormconsultancy.co.uk/random.json",
     success: function(json) {
       console.log( json );
-      $("#quote").html(json.quote);
-      $("#author").html(json.author);
+      $("#quote").html( prepText( json.quote ) );
+      $("#author").html( prepText( json.author ) );
     },
     error: function(error) {
       console.log( error );
