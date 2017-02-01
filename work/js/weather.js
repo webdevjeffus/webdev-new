7a363c0496083e443e5ad43b66f1eecc56f0eb5b
Jeff George <webdevjeffus@gmail.com>
2017-02-01 05:22:39 -0500

    Implement call to weather API; prepare data for temp, wind, skies

--- work/js/weather.js
+++ work/js/weather.js
@@ -1,3 +1,43 @@
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function currentTemp(weatherJSON) {
+  return {
+    "f": Math.round( (1.8 * (weatherJSON.main.temp - 273.15)) + 32 ),
+    "c": Math.round( weatherJSON.main.temp - 273.15 ),
+  };
+}
+
+function currentClouds(weatherJSON) {
+  var clouds = weatherJSON.clouds.all;
+
+  if (clouds <= 10 ) { return "clear"; }
+  else if (clouds <= 30) {return "mostly clear"; }
+  else if (clouds <= 70) {return "partly cloudy"; }
+  else if (clouds <= 90) {return "mostly cloudy"; }
+  else { return "overcast"; }
+}
+
+function currentWind(weatherJSON) {
+  var wind = weatherJSON.wind,
+      result = {};
+
+  if (wind.deg <= 25 || wind.deg > 335 ) { result.direction = "North"}
+  else if ( wind.deg <=  65 ) { result.direction = "Northeast" }
+  else if ( wind.deg <= 115 ) { result.direction = "East" }
+  else if ( wind.deg <= 155 ) { result.direction = "Southeast" }
+  else if ( wind.deg <= 205 ) { result.direction = "South"}
+  else if ( wind.deg <= 245 ) { result.direction = "Southwest" }
+  else if ( wind.deg <= 295 ) { result.direction = "West" }
+  else                        { result.direction = "Northwest "}
+
+  result.speedMPS = Math.round( wind.speed );
+  result.speedMPH = Math.round( wind.speed * 2.23694 );
+
+  return result;
+}
+
 var userIP,
     userLoc,
     userWeather,
@@ -22,9 +62,10 @@ $.getJSON("http://jsonip.com/?callback=?", function (jsonIP, statusIP) {
 
         console.log( locationStr );
 
-        var weatherAPICall = "https://api.openweathermap.org/data/2.5/weather?" + locationStr + "&APPID=d5751e1428d98c3e715937a745922aa3";
+        var weatherAPICall = "http://api.openweathermap.org/data/2.5/weather?" + locationStr + "&APPID=" + weatherKey;
 
         console.log( weatherAPICall );
+
 
         // $.getJSON( weatherAPICall, function( jsonWeather, status ) {
         //    userWeather = jsonWeather;
@@ -36,6 +77,15 @@ $.getJSON("http://jsonip.com/?callback=?", function (jsonIP, statusIP) {
           url: weatherAPICall,
         }).done( function(response) {
           console.log(response);
+          var temp = currentTemp(response),
+              skies = currentClouds(response),
+              wind = currentWind(response)
+
+          $("#tempSpan").text( temp.f + "\xB0 F, " + temp.c + "\xB0 C");
+          $("#skiesSpan").text( capitalize( skies ) );
+          $("#windSpan").text( wind.speedMPH + "mph/" + wind.speedMPS + "ms out of the " + wind.direction );
+
+
         }).fail( function(errors) {
           console.log(errors);
         })
