00b57e54e5803d2380e79d6d443094c9b6378312
Jeff George <webdevjeffus@gmail.com>
2017-02-01 01:52:33 -0500

    Attempt to implement call to weather API; connection refused

--- work/js/weather.js
+++ work/js/weather.js
@@ -1,5 +1,7 @@
 var userIP,
-    userLoc;
+    userLoc,
+    userWeather,
+    weatherKey = "0befa1dbfce1cc5b277b1fc36ecb3de1";
 
 $.getJSON("http://jsonip.com/?callback=?", function (jsonIP, statusIP) {
   userIP = jsonIP.ip;
@@ -15,11 +17,34 @@ $.getJSON("http://jsonip.com/?callback=?", function (jsonIP, statusIP) {
         $("#userTown").text( userLoc.city + ", " + userLoc.region_code + ", " + userLoc.country_code );
         $("#userLat").text( userLoc.latitude );
         $("#userLong").text( userLoc.longitude );
+
+        var locationStr = "lat=" + Math.round(userLoc.latitude) + "&lon=" + Math.round(userLoc.longitude);
+
+        console.log( locationStr );
+
+        var weatherAPICall = "https://api.openweathermap.org/data/2.5/weather?" + locationStr + "&APPID=d5751e1428d98c3e715937a745922aa3";
+
+        console.log( weatherAPICall );
+
+        // $.getJSON( weatherAPICall, function( jsonWeather, status ) {
+        //    userWeather = jsonWeather;
+        //    console.log( userWeather );
+        // });
+
+        $.ajax({
+          method: "GET",
+          url: weatherAPICall,
+        }).done( function(response) {
+          console.log(response);
+        }).fail( function(errors) {
+          console.log(errors);
+        })
+
       }
       else {
         $("#userTown").text( "Unavailable");
       }
-    })
+    });
 
   }
   else {
